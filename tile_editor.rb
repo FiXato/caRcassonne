786edffd3cef6ede022554c8c3cc4b22fb1f35e0
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2010-09-19 03:17:40 +0200

    Rewritten TileEditor to make use of GGLib. It now also allows for showing the properties of a selected tile.

--- tile_editor.rb
+++ tile_editor.rb
@@ -1,68 +1,118 @@
 require 'rubygems'
-require 'gosu'
-require 'rubygems'
 require 'tile'
 require 'tile_set'
 require 'grid'
+require 'gosu'
+require "gglib"
+require "ext/widgets"
+require "ext/themes"
 require "yaml"
-class TileEditor < Gosu::Window
-  attr_accessor :grid, :tile_width, :tile_height, :tile_set
+class TileEditor < GGLib::GUIWindow
+  attr_accessor :grid, :tile_width, :tile_height, :tile_set, :tile_set_name, :board_max_tiles_x, :board_max_tiles_y, :ticks
   attr_reader :width, :height
 
-  def initialize(caption="Tile Editor",width=800,height=600)
-    super(width, height, false)
-    @width = width
-    @height = height
-    self.caption = caption
+  def initialize(caption="Tile Editor",config={})
+    config.keys.each do |k|
+      self.send("#{k}=",config[k]) if self.methods.include?("#{k}=")
+    end
+    tile_set = TileSet.load(tile_set_name)
+    self.tile_set = tile_set
+    self.tile_width = tile_set.tile_dimensions[0]
+    self.tile_height = tile_set.tile_dimensions[1]
+
     @ticks = 0
+    tile_set.uniq_shuffled_tiles!
+    self.grid = Grid.new
+    self.grid.max_x = board_width / tile_width
+    self.grid.max_y = board_height / tile_height
+
+    super(board_width + (5*tile_width), board_height + tile_height, false)
+    self.caption = caption
+    self.state = TileEditorStateObj.new
+    self.add_all_tiles_to_grid
+  end
+
+  def board_width
+    board_max_tiles_x * tile_width
+  end
+
+  def board_height
+    board_max_tiles_y * tile_height
+  end
+
+  def add_all_tiles_to_grid
+    cur_x = 0
+    cur_y = 0
+    tile_set.shuffled_tiles.sort_by{|tile|tile.graphic}.each do |tile|
+      grid.place_tile(tile,cur_x,cur_y,true)
+      cur_x += 1
+      if cur_x > (grid.max_x-1)
+        cur_x = 0
+        cur_y += 1
+      end
+    end
+  rescue OutOfTilesException
+  end
+end
+class TileEditorStateObj < GGLib::StateObject
+  attr_accessor :grid, :tile_set, :tile_width, :tile_height
+  def initialize
+    @grid = $window.grid
+    @tile_set = $window.tile_set
+    @tile_width = $window.tile_width
+    @tile_height = $window.tile_height
+    super
   end
 
   def update
-    @ticks += 1
-    return unless @ticks % 5 == 1
+    $window.ticks += 1
+    return unless $window.ticks % 5 == 1
     @current_tile ||= {
       :grid_x => 0,
       :grid_y => 0,
-      :x => tile_width / 2,
-      :y => tile_height / 2,
-      :image => Gosu::Image.new(self, File.expand_path("resources/tiles/Carcassonne Classic[16 colours]/Outline.png"), true)
+      :x => $window.tile_width / 2,
+      :y => $window.tile_height / 2,
+      :image => Gosu::Image.new($window, File.expand_path("resources/tiles/Carcassonne Classic[16 colours]/Outline.png"), true)
     }
     @selected_tile = nil
-    if button_down? Gosu::Button::KbLeft or button_down? Gosu::Button::GpLeft then
+  rescue OutOfTilesException
+    @current_tile = nil
+  end
+
+  def button_down(id)
+    case id
+    when Gosu::Button::KbLeft, Gosu::Button::GpLeft
       unless @current_tile[:grid_x] == 0
         @current_tile[:grid_x] -= 1 
       end
-    end
-    if button_down? Gosu::Button::KbRight or button_down? Gosu::Button::GpRight then
+    when Gosu::Button::KbRight, Gosu::Button::GpRight
       unless @current_tile[:grid_x] == @grid.max_x - 1
         @current_tile[:grid_x] += 1
       end
-    end
-    if button_down? Gosu::Button::KbDown or button_down? Gosu::Button::GpDown then
+    when Gosu::Button::KbDown, Gosu::Button::GpDown
       unless @current_tile[:grid_y] == @grid.max_y - 1
         @current_tile[:grid_y] += 1 
       end
-    end
-    if button_down? Gosu::Button::KbUp or button_down? Gosu::Button::GpUp then
+    when Gosu::Button::KbUp, Gosu::Button::GpUp
       unless @current_tile[:grid_y] == 0
         @current_tile[:grid_y] -= 1
       end
-    end
-    # if button_down? Gosu::Button::KbSpace or button_down? Gosu::Button::GpButton0 then
-    #   @current_tile[:tile].rotate_clockwise
-    # end
-    if button_down? Gosu::Button::KbReturn or button_down? Gosu::Button::GpButton1 then
+    when Gosu::Button::KbSpace, Gosu::Button::GpButton0
+      @current_tile[:tile].rotate_clockwise
+    when Gosu::Button::KbReturn, Gosu::Button::GpButton1
       puts 'Trying to select tile at %sx%s' % [@current_tile[:grid_x],@current_tile[:grid_y]]
       if @grid.has_tile?(@current_tile[:grid_x],@current_tile[:grid_y])
         @selected_tile = @grid.tile(@current_tile[:grid_x],@current_tile[:grid_y])
+        $txt_north.text = @selected_tile.north.join(", ") if @selected_tile.kind_of?(Tile)
+        $txt_south.text = @selected_tile.south.join(", ") if @selected_tile.kind_of?(Tile)
+        $txt_west.text = @selected_tile.west.join(", ") if @selected_tile.kind_of?(Tile)
+        $txt_east.text = @selected_tile.east.join(", ") if @selected_tile.kind_of?(Tile)
+        $txt_center.text = @selected_tile.center.to_s if @selected_tile.kind_of?(Tile)
       end
-    end
-    if button_down? Gosu::Button::KbBackspace or button_down? Gosu::Button::GpButton2 then
+    when Gosu::Button::KbBackspace, Gosu::Button::GpButton2
       @tile_set.empty_tiles
       @current_tile = nil
     end
-  rescue OutOfTilesException
-    @current_tile = nil
   end
 
   def draw
@@ -75,11 +125,33 @@ class TileEditor < Gosu::Window
         end
       end
     end
-    @grid.draw(self) if @grid
+    @grid.draw($window) if @grid
     @current_tile[:image].draw(@current_tile[:grid_x] * tile_width,@current_tile[:grid_y] * tile_height,0) if @current_tile
   end
-  
+
   def set_background(filename)
     @background_image = Gosu::Image.new(self, filename, true)
+  end
+
+  def onStart
+    puts "TileEditorStateObj activated."
+    $window.setBackground("resources/backgrounds/grey_outline.png")
+    $txt_tileset_name = GGLib::TextBox.new("TileSetName", $window.board_width, 0, 100, GGLib::Themes::Shade,300,50)
+    $txt_tileset_name.text = $window.tile_set_name
+    $txt_north = GGLib::TextBox.new("TileYaml", $window.board_width + 150, 50, 100, GGLib::Themes::Shade,150,50)
+    $txt_south = GGLib::TextBox.new("TileYaml", $window.board_width + 150, 150, 100, GGLib::Themes::Shade,150,50)
+    $txt_west = GGLib::TextBox.new("TileYaml", $window.board_width, 100, 100, GGLib::Themes::Shade,150,50)
+    $txt_east = GGLib::TextBox.new("TileYaml", $window.board_width + 300, 100, 100, GGLib::Themes::Shade,150,50)
+    $txt_center = GGLib::TextBox.new("TileYaml", $window.board_width + 150, 100, 100, GGLib::Themes::Shade,150,50)
+    # GGLib::Button.new("button1", "Save", $window.board_width, 450, Proc.new{ |widget| $txt_tileset_name.text = "Not working yet" }, GGLib::Themes::Shade)
+    GGLib::Button.new("button2", "Exit", 0, $window.board_height, Proc.new{ |widget| $window.close; exit }, GGLib::Themes::Shade)
+  end
+
+  #This method is called right after our state object looses
+  #ownership of the window. The window is automatically reset, but
+  #if you modified anything other than the window, this is where you
+  #should clean it up.
+  def onEnd
+    puts "TileEditorStateObj terminated."
   end
 end
