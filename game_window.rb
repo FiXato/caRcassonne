8191477a08ceeeaa262a03cdd5705374473aee96
Filip H.F. "FiXato" Slagter <fixato@gmail.com>
2011-04-14 23:30:45 +0200

    Added Z-ordering, changed button_down? events for button_up and added sub_grids to tiles.

--- game_window.rb
+++ game_window.rb
@@ -5,6 +5,7 @@ require "yaml"
 class GameWindow < Gosu::Window
   attr_accessor :grid, :tile_width, :tile_height, :tile_set, :save_state_filename, :players, :turn, :current_pawn, :phase
   attr_reader :width, :height, :player_colours
+  ZORDER = {:bg => 0, :tiles => 1, :current_tile => 2, :pawns => 3, :text => 4}
 
   def initialize(caption="Gosu Application",width=800,height=600)
     super(width, height + 200, false)
@@ -41,76 +42,13 @@ class GameWindow < Gosu::Window
     @players_texts ||= players.map{Gosu::Font.new(self,File.expand_path("resources/FixedSysExcelsior300.ttf"), 20)}
   end
 
-  def update
-    @ticks += 1
-    return unless @ticks % 5 == 1
-    if phase == :tile
-      if button_down? Gosu::Button::KbLeft or button_down? Gosu::Button::GpLeft then
-        unless current_tile[:grid_x] == 0
-          current_tile[:grid_x] -= 1 
-        end
-      end
-      if button_down? Gosu::Button::KbRight or button_down? Gosu::Button::GpRight then
-        unless current_tile[:grid_x] == grid.max_x
-          current_tile[:grid_x] += 1
-        end
-      end
-      if button_down? Gosu::Button::KbDown or button_down? Gosu::Button::GpDown then
-        unless current_tile[:grid_y] == grid.max_y
-          current_tile[:grid_y] += 1 
-        end
-      end
-      if button_down? Gosu::Button::KbUp or button_down? Gosu::Button::GpUp then
-        unless current_tile[:grid_y] == 0
-          current_tile[:grid_y] -= 1
-        end
-      end
-      if button_down? Gosu::Button::KbSpace or button_down? Gosu::Button::GpButton0 or button_down? Gosu::Button::MsRight then
-        current_tile[:tile].rotate_clockwise
-        puts "Rotated tile clockwise:"
-        current_tile[:tile].draw_graphs
-      end
-      if button_down? Gosu::Button::KbReturn or button_down? Gosu::Button::GpButton1 or button_down? Gosu::Button::MsLeft then
-        puts 'Trying to place tile at %sx%s' % [current_tile[:grid_x],current_tile[:grid_y]]
-        place_pawn_phase if grid.place_tile(current_tile[:tile],current_tile[:grid_x],current_tile[:grid_y])
-      end
-      if button_down? Gosu::Button::KbS then
-        skip_tile
-      end
-    elsif phase == :pawn
-      if button_down? Gosu::Button::KbLeft or button_down? Gosu::Button::GpLeft then
-        unless current_pawn.sub_grid_position[:x] == 0
-          current_pawn.sub_grid_position[:x] -= 1 
-        end
-      end
-      if button_down? Gosu::Button::KbRight or button_down? Gosu::Button::GpRight then
-        unless current_pawn.sub_grid_position[:x] == 2
-          current_pawn.sub_grid_position[:x] += 1
-        end
-      end
-      if button_down? Gosu::Button::KbDown or button_down? Gosu::Button::GpDown then
-        unless current_pawn.sub_grid_position[:y] == 2
-          current_pawn.sub_grid_position[:y] += 1
-        end
-      end
-      if button_down? Gosu::Button::KbUp or button_down? Gosu::Button::GpUp then
-        unless current_pawn.sub_grid_position[:y] == 0
-          current_pawn.sub_grid_position[:y] -= 1
-        end
-      end
-      if button_down? Gosu::Button::KbReturn or button_down? Gosu::Button::GpButton1 or button_down? Gosu::Button::MsLeft then
-        puts 'Trying to place pawn at %sx%s' % [current_pawn.sub_grid_position[:x],current_pawn.sub_grid_position[:y]]
-        end_turn# if current_player.place_pawn(current_pawn)
-      end
-    end
-    if button_down? Gosu::Button::KbBackspace or button_down? Gosu::Button::GpButton2 then
-      puts "Game was ended prematurely"
-      end_game
-    end
-  rescue OutOfTilesException
-    puts "No more tiles available!" unless @game_ended
-    end_game
-  end
+  # def update
+  #   @ticks += 1
+  #   return unless @ticks % 5 == 1
+  # rescue OutOfTilesException
+  #   puts "No more tiles available!" unless @game_ended
+  #   end_game
+  # end
 
   def draw
     if @background_image
@@ -118,20 +56,20 @@ class GameWindow < Gosu::Window
         offset_x = x_index * tile_width
         (0..(@height/tile_height)).each do |y_index|
           offset_y = y_index * tile_height
-          @background_image.draw(offset_x - 1, offset_y - 1, 0)
+          @background_image.draw(offset_x - 1, offset_y - 1, ZORDER[:bg])
         end
       end
     end
     grid.draw(self)
-    draw_current_tile
-    draw_current_pawn if current_pawn
     players.each{|player|player.placed_pawns.each{|pawn|draw_pawn(pawn)}}
     text = "Turn #{turn}, #{current_player.name}'s move: " if players.size > 0
-    @turn_text.draw(text, 0, height + 10, 0, 1, 1, 0xffffffff)
+    @turn_text.draw(text, 0, height + 10, ZORDER[:text], 1, 1, 0xffffffff)
     players_texts.each_with_index do |font,idx|
       player = players[idx]
-      font.draw(player.name + ' ' * (20-player.name.size) + '☃' * (pawns = player.available_pawns.size) + ('(%s)' % pawns), width / 2, height + 10 + (idx * 20), 0, 1, 1, player_colours[idx])
+      font.draw(player.name + ' ' * (20-player.name.size) + '☃' * (pawns = player.available_pawns.size) + ('(%s)' % pawns), width / 2, height + 10 + (idx * 20), ZORDER[:text], 1, 1, player_colours[idx])
     end
+    draw_current_pawn if current_pawn
+    draw_current_tile
   rescue OutOfTilesException
     puts "No more tiles available!" unless @game_ended
     end_game
@@ -140,11 +78,11 @@ class GameWindow < Gosu::Window
   def draw_current_tile
     x = current_tile[:grid_x] * tile_width + tile_width/2
     y = current_tile[:grid_y] * tile_height + tile_height/2
-    current_tile[:image].draw_rot(x,y,0,current_tile[:tile].rotation) if current_tile
+    current_tile[:image].draw_rot(x,y,ZORDER[:current_tile],current_tile[:tile].rotation) if current_tile
     graphs = current_tile[:tile].to_graphs
     colour = (players.size > 0 ? player_colours[turn % players.size] : 0xffff0000)
     @current_tile_texts.each_with_index do |font,idx|
-      font.draw(graphs[idx].join('').gsub(' ','█').gsub('·', '⃞').gsub('#','❑'), 0, height + 30 + (idx * 40), 0, 1, 1, colour)
+      font.draw(graphs[idx].join('').gsub(' ','█').gsub('·', '⃞').gsub('#','❑'), ZORDER[:text], height + 30 + (idx * 40), 0, 1, 1, colour)
     end
   end
 
@@ -155,7 +93,7 @@ class GameWindow < Gosu::Window
   def draw_pawn(pawn)
     x = (pawn.grid_position[:x] * tile_width) + (pawn.sub_grid_position[:x] * (tile_width/3))
     y = (pawn.grid_position[:y] * tile_height) + (pawn.sub_grid_position[:y] * (tile_height/3))
-    pawn.image.draw(x,y,0)
+    pawn.image.draw(x,y,ZORDER[:pawns])
   end
 
   def set_background(filename)
@@ -170,6 +108,7 @@ class GameWindow < Gosu::Window
     tile = tile_set.get_tile
     puts "New tile taken from stack: "
     tile.draw_graphs
+    puts tile.sub_grid.map{|l|l.join(", ")}.join("\n")
     {
       :tile => tile,
       :image => Gosu::Image.new(self, tile.graphic, true),
@@ -202,7 +141,7 @@ class GameWindow < Gosu::Window
     dup_grid.tiles.each {|tile|tile[2].gosu_image = nil} #kill the gosu images, because they can't be imported.
     save_state = {:grid => dup_grid, :tile_set => tile_set}
     File.open(save_state_filename, "w") { |file| YAML.dump(save_state, file) }
-    # grid.draw_text
+    grid.draw_text
     print "Turn #{turn}"
     print ", #{current_player.name}'s move: " if players.size>0
     print "\n"
@@ -219,9 +158,50 @@ class GameWindow < Gosu::Window
     @game_ended = true
   end
 
-  def button_down(id)
-    if id == Gosu::Button::KbEscape
+  def button_up(id)
+    case id 
+    when Gosu::Button::KbEscape
       close
+    when Gosu::Button::KbBackspace, Gosu::Button::GpButton2
+      puts "Game was ended prematurely"
+      end_game
+    end
+
+    if phase == :tile
+      case id
+      when Gosu::Button::KbLeft, Gosu::Button::GpLeft
+        current_tile[:grid_x] -= 1 unless current_tile[:grid_x] == 0
+      when Gosu::Button::KbRight, Gosu::Button::GpRight
+        current_tile[:grid_x] += 1 unless current_tile[:grid_x] == grid.max_x
+      when Gosu::Button::KbDown, Gosu::Button::GpDown
+        current_tile[:grid_y] += 1 unless current_tile[:grid_y] == grid.max_y
+      when Gosu::Button::KbUp, Gosu::Button::GpUp
+        current_tile[:grid_y] -= 1 unless current_tile[:grid_y] == 0
+      when Gosu::Button::KbSpace, Gosu::Button::GpButton0, Gosu::Button::MsRight
+        current_tile[:tile].rotate_clockwise
+        puts "Rotated tile clockwise:"
+        current_tile[:tile].draw_graphs
+        puts current_tile[:tile].sub_grid.map{|l|l.join(", ")}.join("\n")
+      when Gosu::Button::KbReturn, Gosu::Button::GpButton1, Gosu::Button::MsLeft
+        puts 'Trying to place tile at %sx%s' % [current_tile[:grid_x],current_tile[:grid_y]]
+        place_pawn_phase if grid.place_tile(current_tile[:tile],current_tile[:grid_x],current_tile[:grid_y])
+      when Gosu::Button::KbS
+        skip_tile
+      end
+    elsif phase == :pawn
+      case id
+      when Gosu::Button::KbLeft, Gosu::Button::GpLeft
+        current_pawn.sub_grid_position[:x] -= 1 unless current_pawn.sub_grid_position[:x] == 0
+      when Gosu::Button::KbRight, Gosu::Button::GpRight
+        current_pawn.sub_grid_position[:x] += 1 unless current_pawn.sub_grid_position[:x] == 2
+      when Gosu::Button::KbDown, Gosu::Button::GpDown
+        current_pawn.sub_grid_position[:y] += 1 unless current_pawn.sub_grid_position[:y] == 2
+      when Gosu::Button::KbUp, Gosu::Button::GpUp
+        current_pawn.sub_grid_position[:y] -= 1 unless current_pawn.sub_grid_position[:y] == 0
+      when Gosu::Button::KbReturn, Gosu::Button::GpButton1, Gosu::Button::MsLeft
+        puts 'Trying to place pawn at %sx%s' % [current_pawn.sub_grid_position[:x],current_pawn.sub_grid_position[:y]]
+        end_turn# if current_player.place_pawn(current_pawn)
+      end
     end
   end
 
--- tile.rb
+++ tile.rb
@@ -13,13 +13,24 @@ class Array
   end
 end
 class Tile
-  attr_accessor :id, :type, :graphic, :north, :east, :south, :west, :center, :rotation, :multiplier, :graphic_packed, :gosu_image, :dimensions
+  attr_accessor :north, :east, :south, :west, :center,
+  :id, 
+  :type, 
+  :graphic, :graphic_packed, :gosu_image, 
+  :rotation, :multiplier, 
+  :dimensions, 
+  :pawn, :sub_grid
 
   def initialize(type=nil,graphic=nil)
     self.type = type
     self.graphic = graphic
     self.rotation = 0.0
     self.multiplier = 1
+    @sub_grid = [
+      [[],[],[]],
+      [[],[],[]],
+      [[],[],[]]
+    ]
   end
 
   def rotate_clockwise
@@ -32,6 +43,7 @@ class Tile
     [:north,:east,:south,:west].each do |direction|
       self.send('%s=' % direction,new_directions[direction])
     end
+    self.sub_grid = sub_grid.reverse.transpose
     self.rotation += 90
     self
   end
@@ -41,8 +53,7 @@ class Tile
     self == comparee
   end
 
-  # Objects are equal if they have the same
-  # directions
+  # Objects are equal if they have the same properties
   def ==(comparee)
     self.north == comparee.north && self.east == comparee.east && self.west == comparee.west && self.south == comparee.south && self.center == comparee.center && self.multiplier == comparee.multiplier && self.type == comparee.type
   end
@@ -127,5 +138,51 @@ class Tile
 
   def draw_graphs
     self.to_graphs.each{|graph_line|puts graph_line.join('')}
+  end
+
+  def roads
+    [:north,:east,:south,:west].map{|dir|dir if self.send(dir).road?}.compact
+  end
+
+  def cities
+    [:north,:east,:south,:west].map{|dir|dir if self.send(dir).city?}.compact
+  end
+
+  def meadows
+    [:north,:east,:south,:west].map{|dir|dir if self.send(dir).meadow?}.compact
+  end
+
+  def guess_sub_grid
+    @sub_grid = [
+      [[],[],[]],
+      [[],[],[]],
+      [[],[],[]]
+    ]
+    @sub_grid[0] = [:city,:city,:city] if north.city?
+    @sub_grid[0] = [:meadow,:road,:meadow] if north == [:meadow,:road]
+    @sub_grid[0] = [:meadow,:meadow,:meadow] if north == [:meadow]
+
+    @sub_grid[1][0] = :road if west.road?
+    @sub_grid[1][0] = :city if west.city?
+    @sub_grid[1][0] = :meadow if west == [:meadow]
+    if center.nil? 
+      if (west.road? || east.road? || north.road? || south.road?)
+        @sub_grid[1][1] = :road
+      else
+        @sub_grid[1][1] = :meadow
+      end
+    elsif center.kind_of?(Array)
+      @sub_grid[1][1] = center[0]
+    else
+      @sub_grid[1][1] = center
+    end
+    @sub_grid[1][2] = :road if east.road?
+    @sub_grid[1][2] = :city if east.city?
+    @sub_grid[1][2] = :meadow if east == [:meadow]
+
+    @sub_grid[2] = [:city,:city,:city] if south.city?
+    @sub_grid[2] = [:meadow,:road,:meadow] if south == [:meadow,:road]
+    @sub_grid[2] = [:meadow,:meadow,:meadow] if south == [:meadow]
+    @sub_grid
   end
 end
